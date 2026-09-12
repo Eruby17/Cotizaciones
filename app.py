ac013d5fa62ca4b781d7f648793803f4c44465d4
Eruby17 <94303930+Eruby17@users.noreply.github.com>
2026-09-12 13:58:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5386,20 +5386,7 @@ elif app_mode == "Manual Confirmation":
 
    with mc_col7:
 
-        m_nights = m_calculated_nights
-
-        st.number_input(
-
-            "Nights",
-
-            min_value=1,
-
-            value=m_nights,
-
-            step=1,
-
-            disabled=True,
-        )
+        m_nights = m_calculated_nights st.number_input("Nights", min_value=1, value=m_nights, step=1, disabled=True,)
         
     st.divider()
 
